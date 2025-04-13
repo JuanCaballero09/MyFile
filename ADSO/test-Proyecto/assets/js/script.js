f2b520df162b3877ab2522dd73638e45810e1ef3
JuanCaballero09 <juanes0921200@gmail.com>
2025-04-12 19:02:00 -0500

    update

--- ADSO/test-Proyecto/assets/js/script.js
+++ ADSO/test-Proyecto/assets/js/script.js
@@ -1,27 +1,23 @@
-document.addEventListener('DOMContentLoaded', () => { // Espera a que el DOM esté completamente cargado
-    // Selecciona los elementos del DOM
+document.addEventListener('DOMContentLoaded', () => {
     const section = document.getElementById('section-1');
     const links = document.querySelectorAll('#nav-link');
+    const loginBtn = document.getElementById('login-btn');
+    const userInfo = document.getElementById('user-info');
 
-    // Contenidos simulados
+    // Cargar vista por defecto
     function loadView(viewName){
         fetch(`views/${viewName}.html`)
-        .then(res => res.text())
-        .then(html => {
-            section.innerHTML = html;
-        })
-        .catch(err => {
-            section.innerHTML = '<p>Error al cargar la vista</p><br>';
-            console.error('Error al cargar la vista:', err);
-        });
+            .then(res => res.text())
+            .then(html => section.innerHTML = html)
+            .catch(err => {
+                section.innerHTML = '<p>Error al cargar la vista</p><br>';
+                console.error('Error al cargar la vista:', err);
+            });
     }
 
-    // Cargar menú por defecto
-    loadView('menu');
-    links[0].classList.add('active');
-
+    // Activar links dinámicos
     links.forEach(link => {
-        link.addEventListener('click', (e) => {
+        link.addEventListener('click', e => {
             e.preventDefault();
             const views = link.getAttribute('data-section');
             loadView(views);
@@ -29,4 +25,17 @@ document.addEventListener('DOMContentLoaded', () => { // Espera a que el DOM est
             link.classList.add('active');
         });
     });
+
+    // Cargar vista menú por defecto
+    loadView('menu');
+    links[0].classList.add('active');
+
+    // Login
+    loginBtn.addEventListener('click', () => {
+        loginBtn.classList.add('a');       // Oculta el botón
+        userInfo.classList.remove('a');    // Muestra la info del usuario
+    });
+
+    // Asegúrate de ocultar el usuario por defecto (aunque ya tiene clase .hidden)
+    userInfo.classList.add('hidden');
 });
