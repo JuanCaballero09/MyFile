f136ff44b28a3d217031f07dea421ea5348dda4e
Juan Caballero <juanes0921200@gmail.com>
2025-11-07 09:25:23 -0500

    Migrate react-aprender to new React Vite setup
    Removed the old 'react-aprender' project and added a new React project scaffolded with Vite, including configuration files, source code, and ESLint setup. This migration modernizes the development environment and streamlines tooling for React development.

---
+++ ADSO/React/src/main.jsx
@@ -0,0 +1,12 @@
+import { Fragment, StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { App } from './app.jsx'
+import './main.css'
+
+const root = createRoot(document.getElementById('root'))
+
+
+
+root.render(
+  <App />
+)
